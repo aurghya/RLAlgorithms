6cad85cbeb69c0f45e7bc88e3c07079a14891a7f
aurghya <aurghya@yahoo.com>
2018-12-19 19:57:26 +0530

    experiments for td

--- algorithms/mc.py
+++ algorithms/mc.py
@@ -2,6 +2,19 @@ import sys
 import numpy as np
 from collections import defaultdict
 
+def generate_episode_from_limit_stochastic(bj_env):
+    episode = []
+    state = bj_env.reset()
+    while True:
+        probs = [0.8, 0.2] if state[0] > 18 else [0.2, 0.8]
+        action = np.random.choice(np.arange(2), p=probs)
+        next_state, reward, done, info = bj_env.step(action)
+        episode.append((state, action, reward))
+        state = next_state
+        if done:
+            break
+    return episode
+
 def mc_prediction_q(env, num_episodes, generate_episode, gamma=1.0):
     # initialize empty dictionaries of arrays
     returns_sum = defaultdict(lambda: np.zeros(env.action_space.n))
