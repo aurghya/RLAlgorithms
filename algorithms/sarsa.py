1351fd22ff416fe90c297318c2965acef03da1fa
Aurghya <aurghya.kgp@gmail.com>
2019-06-02 10:45:56 +0530

    sarsa

--- algorithms/sarsa.py
+++ algorithms/sarsa.py
@@ -1,77 +1,64 @@
-import sys
-import gym
 import numpy as np
 import random
-import math
-from collections import defaultdict, deque
-import matplotlib.pyplot as plt
+from collections import defaultdict
 
-def update_Q_sarsa(alpha, gamma, Q, state, action, reward, next_state=None, next_action=None):
-    """Returns updated Q-value for the most recent experience."""
-    current = Q[state][action]  # estimate in Q-table (for current state, action pair)
-    # get value of state, action pair at next time step
-    Qsa_next = Q[next_state][next_action] if next_state is not None else 0    
-    target = reward + (gamma * Qsa_next)               # construct TD target
-    new_value = current + (alpha * (target - current)) # get updated value
-    return new_value
-
-def epsilon_greedy(Q, state, nA, eps):
-    """Selects epsilon-greedy action for supplied state.
-    
-    Params
-    ======
-        Q (dictionary): action-value function
-        state (int): current state
-        nA (int): number actions in the environment
-        eps (float): epsilon
-    """
-    if random.random() > eps: # select greedy action with probability epsilon
+def epsilon_greedy(Q, state, num_actions, eps):
+    if random.random() > eps:
         return np.argmax(Q[state])
-    else:                     # otherwise, select an action randomly
-        return random.choice(np.arange(nA))
+    else:
+        return random.choice(np.arange(num_actions))
 
-def sarsa(env, num_episodes, alpha, gamma=1.0, plot_every=100):
-    nA = env.action_space.n                # number of actions
-    Q = defaultdict(lambda: np.zeros(nA))  # initialize empty dictionary of arrays
-    
-    # monitor performance
-    tmp_scores = deque(maxlen=plot_every)     # deque for keeping track of scores
-    avg_scores = deque(maxlen=num_episodes)   # average scores over every plot_every episodes
-    
-    for i_episode in range(1, num_episodes+1):
-        # monitor progress
-        if i_episode % 100 == 0:
-            print("\rEpisode {}/{}".format(i_episode, num_episodes), end="")
-            sys.stdout.flush()   
-        score = 0                                             # initialize score
-        state = env.reset()                                   # start episode
-        
-        eps = 1.0 / i_episode                                 # set value of epsilon
-        action = epsilon_greedy(Q, state, nA, eps)            # epsilon-greedy action selection
-        
+def sarsa_0(env, num_episodes, alpha, gamma=1.0):
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    for e in range(num_episodes):
+        eps = 1/(e+1)
+
+        state = env.reset()
+        action = epsilon_greedy(Q, state, env.action_space.n, eps)
+
         while True:
-            next_state, reward, done, _ = env.step(action) # take action A, observe R, S'
-            score += reward                                   # add reward to agent's score
-            if not done:
-                next_action = epsilon_greedy(Q, next_state, nA, eps) # epsilon-greedy action
-                Q[state][action] = update_Q_sarsa(alpha, gamma, Q, \
-                                                  state, action, reward, next_state, next_action)
-                
-                state = next_state     # S <- S'
-                action = next_action   # A <- A'
-            if done:
-                Q[state][action] = update_Q_sarsa(alpha, gamma, Q, \
-                                                  state, action, reward)
-                tmp_scores.append(score)    # append score
-                break
-        if (i_episode % plot_every == 0):
-            avg_scores.append(np.mean(tmp_scores))
+            next_state, reward, done, info = env.step(action)
+            next_action = epsilon_greedy(Q, next_state, env.action_space.n, eps)
 
-    # plot performance
-    plt.plot(np.linspace(0,num_episodes,len(avg_scores),endpoint=False), np.asarray(avg_scores))
-    plt.xlabel('Episode Number')
-    plt.ylabel('Average Reward (Over Next %d Episodes)' % plot_every)
-    plt.show()
-    # print best 100-episode performance
-    print(('Best Average Reward over %d Episodes: ' % plot_every), np.max(avg_scores))    
+            Q[state][action] = Q[state][action] + alpha*(reward + gamma*Q[next_state][next_action] - Q[state][action])
+            state = next_state
+            action = next_action
+            
+            if done:
+                break
+
     return Q
+
+def sarsa_lambda(env, val_lambda, num_episodes, alpha, gamma=1.0):
+    Q_new = defaultdict(lambda: np.zeros(env.action_space.n))
+    Q_old = defaultdict(lambda: np.zeros(env.action_space.n))
+
+    for e in range(num_episodes):
+        eps = 1/(e+1)
+        E = defaultdict(lambda: np.zeros(env.action_space.n))
+
+        state = env.reset()
+        action = epsilon_greedy(Q_old, state, env.action_space.n, eps)
+
+        while True:
+            next_state, reward, done, info = env.step(action)
+            next_action = epsilon_greedy(Q_old, next_state, env.action_space.n, eps)
+            
+            delta = reward + gamma*Q_old[next_state][next_action] - Q_old[state][action]
+
+            E[state][action] += 1
+            for s in range(env.observation_space.n):
+                for a in range(env.action_space.n):
+                    Q_new[s][a] = Q_new[s][a] + alpha*delta*E[s][a]
+                    E[s][a] = gamma*val_lambda*E[s][a]
+
+            state = next_state
+            action = next_action
+
+            Q_old = Q_new.copy()
+            
+            if done:
+                break
+
+    return Q_new
